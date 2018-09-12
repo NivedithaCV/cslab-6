6a94fce5f0db07b43d336dc9b279311000223c32
NivedithaCV <42238305+NivedithaCV@users.noreply.github.com>
2018-09-12 09:58:59 -0400

    Add files via upload

---
+++ lab6_q4.cpp
@@ -0,0 +1,26 @@
+//(By value) minimum
+//Library
+#include<iostream>
+using namespace std;
+//declaring function
+int func(int a,int b,int c){
+	//giving condition
+	if(a>b)
+	c=b;
+	else if(b>a)
+	c=a;
+	
+	return c; 
+}
+//drive function
+int main(){
+	//declaring variables
+	int a,b,c;
+	//asking for integers
+	cout<<"write two numbers"<<endl;
+	cin>>a;
+	cin>>b;
+	//printing the minimum
+	cout<<"the minimum of these is "<<func(a,b,c)<<endl;
+return 0;
+}	
