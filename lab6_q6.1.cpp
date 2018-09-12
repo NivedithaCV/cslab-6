d70688a563e38b17afd4da8c7f1f0507a80fc51e
NivedithaCV <42238305+NivedithaCV@users.noreply.github.com>
2018-09-12 17:55:29 +0000

    Update lab6_q6.1.cpp

--- lab6_q6.1.cpp
+++ lab6_q6.1.cpp
@@ -8,9 +8,9 @@ int sumEvenNumbers(int a,int b)
 {  
   int sum=0;
 //looping
- for(int i=a;i<=b;i++)
+ for(int i=a;i<b;i++)
 	//condition for even numbers
-	{if(i%2==0)
+	{if(i%2==0&&i>a)
 	//adding
 	{ sum=sum+i;}}
 	return sum;}
